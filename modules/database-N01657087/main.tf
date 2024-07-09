02c3276dce6648f58a6e6c8681a4ba3c434ca94b
ashnapaularackal <99412166+ashnapaularackal@users.noreply.github.com>
2024-07-08 20:37:27 -0400

    Update main.tf

--- modules/database-N01657087/main.tf
+++ modules/database-N01657087/main.tf
@@ -3,14 +3,14 @@ resource "azurerm_postgresql_server" "postgresql" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  sku_name            = "B_Gen5_1"  # Choose the SKU as per your requirements
-  storage_mb          = 5120        # Set storage in MB
+  sku_name            = "B_Gen5_1"  
+  storage_mb          = 5120        
   backup_retention_days = 7
-  version             = "11"        # Choose the PostgreSQL version
+  version             = "11"        
 
   administrator_login = var.administrator_login
   administrator_login_password = var.administrator_login_password
-  ssl_enforcement_enabled = true   # Required argument for PostgreSQL server
+  ssl_enforcement_enabled = true  
 
   tags = {
     Assignment     = var.assignment
